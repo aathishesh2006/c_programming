4251364072fa08d52444e41bdcdf0092a66258c7
aathishesh2006 <sachinaathishesh@gmail.com>
2026-08-21 20:18:09 +0530

    modified

--- day 25/tictactoe.c
+++ day 25/tictactoe.c
@@ -1,1 +1,26 @@
 #include <stdio.h>
+
+int slot_size()
+{
+    int size;
+    printf("To enter the board size :");
+    scanf("%d", size);
+    return size;
+}
+void title_of_slot_size(int size)
+{
+    printf("\n\n %d X %d slot \n\n", size,size);
+
+}
+void slot(int size, int arr[size][size])
+{
+    int element = 1;
+    for (int i = 0; i < size; i++)
+    {
+        for(int j = 0; j < size; j++)
+        {
+            arr[i][j] = element++;
+        }
+    } 
+}
+void slot_
