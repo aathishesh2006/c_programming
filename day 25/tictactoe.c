c731f88d9619dc407b0d2e74840e29ad3094eb68
aathishesh2006 <sachinaathishesh@gmail.com>
2026-08-24 12:42:05 +0530

    modified

--- day 25/tictactoe.c
+++ day 25/tictactoe.c
@@ -19,29 +19,30 @@ void slot(int size, int arr[size][size])
     {
         for(int j = 0; j < size; j++)
         {
-            arr[i][j] = element++;
+        arr[i][j] = element++;
         }
     } 
 }
-void slot(int size, int arr[size][size])
+void slot_output(int size, int arr[size][size])
 {
-    int element = 1;
     for (int i = 0; i < size; i++)
     {
         for(int j = 0; j < size; j++)
         {
-            arr[i][j] = element++;
-        }
-    } 
-}
-void select(int size, int arr[size][size])
-{
-    int element = 2;
-    for (int i = 0; i < size; i++)
-    {
-        for(int j = 0; j < size; j++)
+        if(arr[i][j] == -1)
         {
-            arr[i][j] = element++;
+            printf(" O ");
+        }    
+        else if(arr[i][j]==-2)
+        {
+            printf(" X ");
+        }
+        else
+        {
+            printf(" %02d ",arr[i][j]);
         }
     }
+    printf("\n");
+    } 
 }
+
