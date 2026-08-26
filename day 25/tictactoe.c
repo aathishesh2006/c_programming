55257ed904fbedb556f50a54617ac2e007554505
aathishesh2006 <sachinaathishesh@gmail.com>
2026-08-26 11:27:31 +0530

    modified

--- day 25/tictactoe.c
+++ day 25/tictactoe.c
@@ -3,130 +3,204 @@
 int slot_size()
 {
     int size;
-    printf("To enter the board size :");
-    scanf("%d", size);
+    printf("To enter the board size : ");
+    scanf("%d", &size);
     return size;
 }
+
 void title_of_slot_size(int size)
 {
-    printf("\n\n %d X %d slot \n\n", size,size);
-
+    printf("\n\n %d X %d slot \n\n", size, size);
 }
+
 void slot(int size, int arr[size][size])
 {
     int element = 1;
     for (int i = 0; i < size; i++)
     {
-        for(int j = 0; j < size; j++)
-        {
-        arr[i][j] = element++;
-        }
-    } 
+    for (int j = 0; j < size; j++)
+    {
+    arr[i][j] = element++;
+    }
+    }
 }
+
 void slot_output(int size, int arr[size][size])
 {
     for (int i = 0; i < size; i++)
     {
-        for(int j = 0; j < size; j++)
-        {
-        if(arr[i][j] == -1)
-        {
-            printf(" O ");
-        }  
-        else if(arr[i][j]==-2)
-        {
-            printf(" X ");
-        }
-        else
-        {
-            printf(" %02d ",arr[i][j]);
-        }
+    for (int j = 0; j < size; j++)
+    {
+    if (arr[i][j] == -1)
+    {
+    printf(" O ");
+    }
+    else if (arr[i][j] == -2)
+    {
+    printf(" X ");
+    }
+    else
+    {
+    printf(" %02d ", arr[i][j]);
+    }
     }
     printf("\n");
     }
 }
+
 void gamestart(int size, int arr[size][size], int element)
 {
     int x, y, a, b;
-while(1)
-{
-    peintf("\n Enter here %s: ", (element == -1)? "O" : "X");
+    while (1)
+    {
+    printf("\nEnter here %s: ",
+    (element == -1) ? "O" : "X");
     scanf("%d", &x);
 
-    y = a - 1;
+    if (x < 1 || x > size * size)
+    {
+    printf("Invalid position...\n");
+    continue;
+    }
+
+    y = x - 1;
     a = y / size;
     b = y % size;
 
-    if(arr[a][b] == -1, arr[a][b] == -2)
+    if (arr[a][b] == -1 || arr[a][b] == -2)
     {
-        printf("Occupied..\n");
+    printf("Occupied...\n");
     }
     else
     {
-        arr[a][b] = element;
-        break;
+    arr[a][b] = element;
+    break;
+    }
     }
 }
-}
+
 int gamerule(int size, int arr[size][size], int element)
 {
     int rows, columns;
     int win;
 
-
-    for(rows = 0; rows < size; rows ++)
+    for (rows = 0; rows < size; rows++)
     {
-        win = 1;
-        for(columns = 0; columns < size; columns ++)
-        {
-            if(arr[rows][columns] != element)
-            {
-                win = 0;
-                break;
-            }
-        }
-        if(win == 1)
-        return 1;
+    win = 1;
+
+    for (columns = 0; columns < size; columns++)
+    {
+    if (arr[rows][columns] != element)
+    {
+    win = 0;
+    break;
+    }
     }
 
-
-    for(columns = 0; columns < size; columns ++)
+    if (win == 1)
     {
-        win = 1;
-        for(rows = 0; rows < size; rows ++)
-        {
-            if(arr[rows][columns] != element)
-            {
-                win = 0;
-                break;
-            }
-        }
-        if(win == 1)
-        return 1;
+    return 1;
+    }
     }
 
-win = 1;
-for (rows = 0; rows < size; rows++)
-{
-    if(arr[rows][rows] != element)
+    for (columns = 0; columns < size; columns++)
+    {
+    win = 1;
+    for (rows = 0; rows < size; rows++)
+    {
+    if (arr[rows][columns] != element)
+        {
+        win = 0;
+        break;
+        }
+    }
+
+    if (win == 1)
+    {
+        return 1;
+    }
+    }
+
+    win = 1;
+
+    for (rows = 0; rows < size; rows++)
+    {
+    if (arr[rows][rows] != element)
     {
         win = 0;
         break;
     }
-}
-if(win == 1)
-return 1;
+    }
 
-win = 1;
-for(rows = 0; rows < size; rows++)
-{
-    if(arr[columns][columns] != element)
+    if (win == 1)
+    {
+        return 1;
+    }
+
+    win = 1;
+
+    for (rows = 0; rows < size; rows++)
+    {
+    if (arr[rows][size - 1 - rows] != element)
     {
         win = 0;
         break;
     }
-}
-if (win == 1)
-return 1;
+    }
+
+    if (win == 1)
+    {
+        return 1;
+    }
+
+    return 0;
 }
 
+int main()
+{
+    int size;
+    size = slot_size();
+    title_of_slot_size(size);
+
+    int arr[size][size];
+    slot(size, arr);
+    slot_output(size, arr);
+
+    int element;
+
+    int over_size = size * size;
+    for (int rows = 0; rows < over_size; rows++)
+    {
+        if (rows % 2 == 0)
+        {
+            element = -1;       
+        }
+        else
+        {
+            element = -2;       
+        }
+        gamestart(size, arr, element);
+        slot_output(size, arr);
+
+        if (gamerule(size, arr, element))
+        {
+        if (element == -1)
+        {
+        printf("\nPlayer O won\n");
+        }
+        else
+        {
+        printf("\nPlayer X won\n");
+        }
+
+        break;
+        }
+
+        if (rows == over_size - 1)
+        {
+        printf("\nDraw\n");
+        }
+    }
+
+return 0;
+}
