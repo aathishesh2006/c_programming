4f46a9e68e8e211c4a2ab904764d3f2c0bffddc6
aathishesh2006 <sachinaathishesh@gmail.com>
2026-08-26 18:40:39 +0530

    modified

---
+++ day 26/flames.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+void input_name()
+{
+    printf("Enter the first name :");
+    scanf("%c", input_name);
+    printf("Enter the second name :");
+    scanf("%c", input_name);
+}
+
+
