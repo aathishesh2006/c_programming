6bff1cb006fe931a5d99a6cefd84e80ee8e871dd
aathishesh2006 <sachinaathishesh@gmail.com>
2026-08-28 18:34:16 +0530

    modified

--- day 26/flames.c
+++ day 26/flames.c
@@ -1,86 +1,72 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 void getnames(char name_1[], char name_2[])
 {
-    printf("Enter the name_1 :");
-    scanf("%s", name_1);
+    printf("Enter the name_1 : ");
+    scanf("%49s", name_1);
 
     printf("\n");
 
-    printf("Enter the name_2 :");
-    scanf("%s", name_2);
+    printf("Enter the name_2 : ");
+    scanf("%49s", name_2);
 
     printf("\n");
 
-if (strcmp(name_1, name_2) == 0)
-{
-    printf("INVALID.\n");
+    if (strcmp(name_1, name_2) == 0)
+    {
+        printf("INVALID...\n");
+    }
+    else
+    {
+        printf("Welcome To FLAMES game !!!\n");
+        printf("%s and %s\n", name_1, name_2);
+    }
 }
-else
-{
-    printf("Welcome To FLAMES game !!!\n");
-    printf("%s and %s\n", name_1, name_2);
-}
-}
+
 
 int name_len(char name_1[], char name_2[])
 {
-int len1 = strlen(name_1);
-int len2 = strlen(name_2);
+    int len1 = strlen(name_1);
+    int len2 = strlen(name_2);
 
-char result[100];
-
-int found, i, j;
-int k = 0;
-
-for (i = 0; i < len2; i++)
-{
-    found = 0;
-
-    for (j = 0; j < len1; j++)
-    {
-    if (name_2[i] == name_1[j])
-    {
-        found = 1;
-        break;
-    }
-    }
-
-    if (found == 0)
-    {
-        result[k] = name_2[i];
-        k++;
-    }
-    }
+    int used[50] = {0};
+    int common = 0;
+    int i, j;
 
     for (i = 0; i < len1; i++)
     {
-        found = 0;
+        name_1[i] = tolower((unsigned char)name_1[i]);
+    }
 
+    for (i = 0; i < len2; i++)
+    {
+        name_2[i] = tolower((unsigned char)name_2[i]);
+    }
+
+
+    for (i = 0; i < len1; i++)
+    {
     for (j = 0; j < len2; j++)
     {
-    if (name_1[i] == name_2[j])
-    {
-        found = 1;
+        if (name_1[i] == name_2[j] && used[j] == 0)
+        {
+        common++;
+        used[j] = 1;
         break;
+        }
     }
     }
 
-    if (found == 0)
-    {
-        result[k] = name_1[i];
-        k++;
-    }
-    }
+    int total_length = (len1 - common) + (len2 - common);
 
-    result[k] = '\0';
-
-    int total_length = strlen(result);
+    printf("Common characters = %d\n", common);
     printf("Remaining count = %d\n", total_length);
 
     return total_length;
 }
+
 
 void flames(int total_length)
 {
@@ -90,42 +76,59 @@ void flames(int total_length)
         return;
     }
 
-    int result = total_length % 6;
+    char flames[] = {'F', 'L', 'A', 'M', 'E', 'S'};
 
-    if (result == 0)
+    int size = 6;
+    int index = 0;
+
+    while (size > 1)
     {
-        result = 6;
+    index = (index + total_length - 1) % size;
+
+    for (int i = index; i < size - 1; i++)
+    {
+        flames[i] = flames[i + 1];
     }
+
+    size--;
+
+    if (index == size)
+    {
+        index = 0;
+    }
+    }
+
 
     printf("\n");
 
-    switch (result)
+    switch (flames[0])
     {
-        case 1:
+        case 'F':
             printf("FLAMES Result : FRIENDS\n");
             break;
 
-        case 2:
+        case 'L':
             printf("FLAMES Result : LOVE\n");
             break;
 
-        case 3:
+        case 'A':
             printf("FLAMES Result : AFFECTION\n");
             break;
 
-        case 4:
+        case 'M':
             printf("FLAMES Result : MARRIAGE\n");
             break;
 
-        case 5:
+        case 'E':
             printf("FLAMES Result : ENEMY\n");
             break;
 
-        case 6:
+        case 'S':
             printf("FLAMES Result : SIBLINGS\n");
             break;
     }
 }
+
 
 int main()
 {
@@ -137,6 +140,7 @@ int main()
     int total_length;
 
     total_length = name_len(name_1, name_2);
+
     flames(total_length);
 
     return 0;
