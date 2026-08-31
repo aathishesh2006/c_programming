fd5e8f086281192963a43e5b456c79fa8c6d2632
aathishesh2006 <sachinaathishesh@gmail.com>
2026-08-31 12:56:59 +0530

    modified

--- day 27/linkedlist.c
+++ day 27/linkedlist.c
@@ -36,7 +36,7 @@ struct node *createlist()
     printf("Enter the number of node :");
     scanf("%d", &n);
 
-    for(int i =0; i < n; i++)
+    for(int i=0; i<n; i++)
     {
         printf("Enter the value of the node %d: ", i + 1);
         scanf("%d", & value);
