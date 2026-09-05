db9cbbeb8f34ab2cff9d75efb572dfde6063e428
aathishesh2006 <sachinaathishesh@gmail.com>
2026-09-05 11:18:47 +0530

    modified

--- day 03/ifelseif.c
+++ day 03/ifelseif.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 int main()
 {
-    int arun = 21;
+    int arun = 2;
     if(arun>=18)
-    printf("tou are now elligible");
-    else if(arun<=15)
+    printf("you are now elligible");
+    else if(arun<=10 || arun<=15)
     printf("you are not elligable");
-    else if(arun<=10)
+    else if(arun<=6 || arun<=9)
     printf("grow and come");
     else 
     printf("you are a kid lol");
